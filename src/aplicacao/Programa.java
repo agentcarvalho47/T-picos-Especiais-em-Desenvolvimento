0d0b676b3abc622c437e0a577642b3d8a271870d
agentcarvalho47 <carvalhoandre568@gmail.com>
2019-04-30 17:26:56 -0300

    Segundo Commit

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -15,10 +15,8 @@ public class Programa {
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		String jpql = "SELECT p FROM Pessoa p";
-		List<Pessoa> pessoas = em.createQuery(jpql, Pessoa.class).getResultList();
 		Scanner s = new Scanner(System.in);
-		
+		Pessoa p = new Pessoa();
 		int e = 0;
 
 		do {
@@ -31,77 +29,19 @@ public class Programa {
 			e = s.nextInt();
 			switch (e) {
 			case 1:
-				pessoas = em.createQuery(jpql, Pessoa.class).getResultList();
-				System.out.println(pessoas);
+				p.listarPessoas(em);
 				break;
-			case 2:
-				System.out.print("Digite o id: ");
-				int id = s.nextInt();				
-				Pessoa p = em.find(Pessoa.class, id);
-				if(p == null) {
-					System.out.println("id não cadastrado!");
-				}
-				else {
-					System.out.println("Pessoa encontrada!");
-					System.out.println(p);					
-				}
+			case 2:			
+				p.buscarId(em);
 				break;
 			case 3:
-				System.out.println("Digite o nome: ");
-				String nome = s.next();
-				System.out.println("Digite o email: ");
-				String email = s.next();
-				em.getTransaction().begin();
-				Pessoa pessoa = new Pessoa(null, nome, email);
-				em.persist(pessoa);
-				em.getTransaction().commit();
-				System.out.println("Pessoa cadastrada!");
+				p.cadastrarPessoa(em);
 				break;
 			case 4:
-				System.out.println("Digite o id: ");
-				int id1 = s.nextInt();
-				Pessoa pFound = em.find(Pessoa.class, id1);
-				if(pFound == null) {
-					System.out.println("id não cadastrado!");
-				}
-				else {
-					System.out.println("Escolha o campo para atualizar: 1 - Nome, 2 - Email");
-					int e1 = s.nextInt();
-					switch (e1) {
-					case 1:
-						String nomeNovo = s.next();
-						pFound.setNome(nomeNovo);
-						em.getTransaction().begin();
-						em.persist(pFound);
-						em.getTransaction().commit();
-						break;
-					case 2:
-						String emailNovo = s.next();
-						pFound.setEmail(emailNovo);
-						em.getTransaction().begin();
-						em.persist(pFound);
-						em.getTransaction().commit();
-						break;
-					default:
-						System.out.println("Opção Inválida!");
-						break;
-					}
-					System.out.println("Atualizado!");
-				}
+				p.atualizarPessoa(em);
 				break;
 			case 5:
-				System.out.println("Digite o id: ");
-				int id2 = s.nextInt();
-				Pessoa pfound = em.find(Pessoa.class, id2);
-				if(pfound == null) {
-					System.out.println("id não cadastrado!");
-				}
-				else {
-					em.getTransaction().begin();
-					em.remove(pfound);
-					em.getTransaction().commit();
-					System.out.println("Removido!");					
-				}
+				p.removerPessoa(em);
 				break;
 			case 0:
 				break;
--- src/dominio/Pessoa.java
+++ src/dominio/Pessoa.java
@@ -1,8 +1,11 @@
 package dominio;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Scanner;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -18,7 +21,8 @@ public class Pessoa implements Serializable {
 	private Integer id;
 	private String nome;
 	private String email;
-
+	
+	
 	public Pessoa(Integer id, String nome, String email) {
 		this.id = id;
 		this.nome = nome;
@@ -28,7 +32,6 @@ public class Pessoa implements Serializable {
 	public Pessoa() {
 		
 	}
-
 	public Integer getId() {
 		return id;
 	}
@@ -52,7 +55,86 @@ public class Pessoa implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	
+	public void listarPessoas(EntityManager em) {
+		String jpql = "SELECT p FROM Pessoa p";
+		List<Pessoa> pessoas = em.createQuery(jpql, Pessoa.class).getResultList();
+		pessoas = em.createQuery(jpql, Pessoa.class).getResultList();
+		System.out.println(pessoas);
+	}
+	public void buscarId(EntityManager em) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Digite o id: ");
+		int id = s.nextInt();
+		Pessoa p = em.find(Pessoa.class, id);
+		if(p == null) {
+			System.out.println("id não cadastrado!");
+		}
+		else {
+			System.out.println("Pessoa encontrada!");
+			System.out.println(p);					
+		}
+	}
+	public void cadastrarPessoa(EntityManager em) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Digite o nome: ");
+		String nome = s.next();
+		System.out.println("Digite o email: ");
+		String email = s.next();
+		em.getTransaction().begin();
+		Pessoa pessoa = new Pessoa(null, nome, email);
+		em.persist(pessoa);
+		em.getTransaction().commit();
+		System.out.println("Pessoa cadastrada!");
+	}
+	public void atualizarPessoa(EntityManager em) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Digite o id: ");
+		int id1 = s.nextInt();
+		Pessoa pFound = em.find(Pessoa.class, id1);
+		if(pFound == null) {
+			System.out.println("id não cadastrado!");
+		}
+		else {
+			System.out.println("Escolha o campo para atualizar: 1 - Nome, 2 - Email");
+			int e1 = s.nextInt();
+			switch (e1) {
+			case 1:
+				String nomeNovo = s.next();
+				pFound.setNome(nomeNovo);
+				em.getTransaction().begin();
+				em.persist(pFound);
+				em.getTransaction().commit();
+				break;
+			case 2:
+				String emailNovo = s.next();
+				pFound.setEmail(emailNovo);
+				em.getTransaction().begin();
+				em.persist(pFound);
+				em.getTransaction().commit();
+				break;
+			default:
+				System.out.println("Opção Inválida!");
+				break;
+			}
+			System.out.println("Atualizado!");
+		}
+	}
+	public void removerPessoa(EntityManager em) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Digite o id: ");
+		int id2 = s.nextInt();
+		Pessoa pfound = em.find(Pessoa.class, id2);
+		if(pfound == null) {
+			System.out.println("id não cadastrado!");
+		}
+		else {
+			em.getTransaction().begin();
+			em.remove(pfound);
+			em.getTransaction().commit();
+			System.out.println("Removido!");					
+		}
+	}
 	@Override
 	public String toString() {
 		return "Pessoa [id=" + id + ", nome=" + nome + ", email=" + email + "]";
